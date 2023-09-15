815d401c3e0e702d1991253b161a7551cd166078
Aswinraj <aswinrajr07@gmail.com>
2023-09-15 23:52:05 +0530

    db string updated

--- Database/db.js
+++ Database/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const dbConnect = (req,res)=>{
   // mongoose.connect("mongodb://127.0.0.1:27017/Project_Ecommerse", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
  
- mongoose.connect("mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/Project_Ecommerse").then(()=> {
-  console.log("connected to the database")
+ mongoose.connect("mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/Project_Ecommerse", { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> {
+  console.log("connected to the database atlas")
 }).catch((err) => {
   console.error("Error in connecting the database", err)
 })
